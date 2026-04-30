d6d30044db0d327dcc1430daa58ac523fe4b798f
brownm09 <36768444+brownm09@users.noreply.github.com>
2026-04-30 11:21:14 -0400

    [fix] Add week field to mapLiftingProgramSpec test fixtures

--- packages/core/tests/core/utils/mapper/mapLiftingProgramSpec.test.ts
+++ packages/core/tests/core/utils/mapper/mapLiftingProgramSpec.test.ts
@@ -7,6 +7,7 @@ import {
 
 const specs: LiftingProgramSpec[] = [
   {
+    week: 1,
     offset: 0,
     lift: "Squat",
     increment: 5,
@@ -19,6 +20,7 @@ const specs: LiftingProgramSpec[] = [
     activation: "None",
   },
   {
+    week: 1,
     offset: 0,
     lift: "Bench",
     increment: 2.5,
@@ -37,8 +39,8 @@ describe("mapLiftingProgramSpec", () => {
     const headers = Object.keys(LIFTING_PROGRAM_SPEC_HEADER_MAP);
     const result = mapLiftingProgramSpec(specs);
     expect(result[0]).toEqual(headers);
-    expect(result[1]).toEqual([0, "Squat", 5, 1, 3, 5, "TRUE", "40,50,60", 0, "None"]);
-    expect(result[2]).toEqual([0, "Bench", 2.5, 2, 3, 5, "FALSE", "40,50,60", 0, "None"]);
+    expect(result[1]).toEqual([1, 0, "Squat", 5, 1, 3, 5, "TRUE", "40,50,60", 0, "None"]);
+    expect(result[2]).toEqual([1, 0, "Bench", 2.5, 2, 3, 5, "FALSE", "40,50,60", 0, "None"]);
   });
 
   it("maps amrap boolean to string so parseLiftingProgramSpec can round-trip it", () => {
